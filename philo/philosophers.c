b9431b8736e405da4612a2847286c0801ca21ed8
nytsdayo <nyantsutsu@gmail.com>
2024-10-08 07:48:27 +0900

    reformat

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -4,48 +4,53 @@
 #include <sys/time.h>
 #include "philosophers.h"
 #include "struct.h"
+#include <stdlib.h>
 
 int cnt = 0;
 
-long month_calculator(time_t now_time, int year)
+long *get_month_sec()
 {
-    int now_month;
+    long *month_sec;
     int i;
-    long month_sec[12];
-
+    month_sec = (long *)malloc(sizeof(long) * 12);
     i = 0;
     while(i < 12)
     {
         if (i % 2 == 0)
-        {
             if(i == 2)
-                if (year % 4 == 0 && year % 100 != 0)
-                    month_sec[i] = 29 * YEAR_SEC;
+                if (cnt % 4 == 0 && cnt % 100 != 0)
+                    month_sec[i] = 29 * DAY_SEC;
                 else
-                    month_sec[i] = 28 * YEAR_SEC;
+                    month_sec[i] = 28 * DAY_SEC;
             else if(i < 7)
-                    month_sec[i] = 30 * YEAR_SEC;
+                    month_sec[i] = 30 * DAY_SEC;
             else
-                month_sec[i] = 31 * YEAR_SEC;
-        }
-            month_sec[i] = 2592000;
-        if (i == 1)
-        {
-            if (year % 4 == 0 && year % 100 != 0)
-                month_sec[i] = 2505600;
+                month_sec[i] = 31 * DAY_SEC;
+        else
+            if(i < 7)
+                month_sec[i] = 31 * DAY_SEC;
             else
-                month_sec[i] = 2419200;
-        }
+                month_sec[i] = 30 * DAY_SEC;
         i++;
     }
+    return (month_sec);
+}
 
+int month_calculator(time_t now_time, int year)
+{
+    int now_month;
+    int i;
+    long *month_sec;
+
+    i = 0;
+    month_sec = get_month_sec();
 
     while(year > 1970)
     {
         if (year % 4 == 0)
-            now_time -= 366 * (long)YEAR_SEC;
+            now_time -= 366 * DAY_SEC;
         else
-            now_time -= 365 * (long)YEAR_SEC;
+            now_time -= 365 * DAY_SEC;
         year--;
     }
     i = 0;
@@ -54,6 +59,7 @@ long month_calculator(time_t now_time, int year)
         now_time -= month_sec[i];
         i++;
     }
+    free(month_sec);
     return (i + 1);
 }
 
@@ -61,8 +67,9 @@ int day_calculator(time_t now_time, int year, int month)
 {
     int now_month;
     int i;
-    int month_sec[12] = {2678400, 2419200, 2678400, 2592000, 2678400, 2592000, 2678400, 2678400, 2592000, 2678400, 2592000, 2678400};
+    long *month_sec;
 
+    month_sec = get_month_sec();
     i = 0;
     while(year > 1970)
     {
@@ -78,6 +85,7 @@ int day_calculator(time_t now_time, int year, int month)
         now_time -= month_sec[i];
         i++;
     }
+    free(month_sec);
     return (now_time / 86400 + 1);
 }
 
@@ -104,7 +112,7 @@ t_timestamp get_timestamp()
     timestamp.year = 1970 + now_time.tv_sec / 31556926;
     timestamp.month = month_calculator(now_time.tv_sec, timestamp.year);
     timestamp.day = day_calculator(now_time.tv_sec, timestamp.year, timestamp.month);
-    timestamp.hour = hour_calculator(now_time.tv_sec + TIME_DIFFERENCE * HOUR_SEC);
+    timestamp.hour = hour_calculator(now_time.tv_sec + (TIME_DIFFERENCE * HOUR_SEC));
     timestamp.minute = minute_calculator(now_time.tv_sec);
     timestamp.second = second_calculator(now_time.tv_sec);
     timestamp.millisecond = now_time.tv_usec / 1000;
